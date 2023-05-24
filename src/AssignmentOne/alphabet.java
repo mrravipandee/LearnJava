b0119f532402854885896faffec35ec539faff3f
Ravi Pandey <imravipanday@gmail.com>
2023-05-24 12:16:46 +0530

    normal program practices.

---
+++ src/AssignmentOne/alphabet.java
@@ -0,0 +1,10 @@
+package AssignmentOne;
+
+public class alphabet {
+    public static void main(String[] args) {
+
+        for (char alpha = 'A'; alpha <= 'Z'; alpha++) {
+            System.out.println(alpha);
+        }
+    }
+}
---
+++ src/AssignmentOne/radhe.java
@@ -0,0 +1,14 @@
+package AssignmentOne;
+import java.util.Scanner;
+
+public class radhe {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+        sc.close();
+
+    }
+}
