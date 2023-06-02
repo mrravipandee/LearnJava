e052ad3a49d261e2d27f88f38b0632c8428ddb15
Ravi Pandey <imravipanday@gmail.com>
2023-06-02 23:50:35 +0530

    Color text in java -> command line output.

---
+++ src/AssignmentOne/radheRose.java
@@ -0,0 +1,17 @@
+package AssignmentOne;
+
+public class radheRose {
+
+    public static void main(String[] args) {
+
+        System.out.println("\u001B[31m Roses are red");
+        System.out.println("\u001B[34m Violets are blue");
+        System.out.println("\u001B[30m I don't sleep at night");
+        System.out.println("\u001B[35m cause I'm thinking of you");
+        System.out.println("\u001B[37m Alone with my thoughts");
+        System.out.println("\u001B[32m trapped in this bed");
+        System.out.println("\u001B[36m Know I'd give the world");
+        System.out.println("\u001B[33m just to see you again.");
+
+    }
+}
