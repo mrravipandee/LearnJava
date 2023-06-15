6f08cb1bc2362ff10d885cfc246cdc43b1f7ec92
Ravi Pandey <imravipanday@gmail.com>
2023-06-15 15:42:58 +0530

    WAP for check current time and date.

---
+++ src/AssignmentOne/CheckTimeDate.java
@@ -0,0 +1,11 @@
+package AssignmentOne;
+
+import java.time.LocalDateTime;
+
+public class CheckTimeDate {
+
+    public static void main(String[] args) {
+        LocalDateTime myObj = LocalDateTime.now();
+        System.out.println(myObj);
+    }
+}
